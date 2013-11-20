438538dfa809be18d5c89608d47e12f34eea8aae
UtkarshGupta <utkarsh.eminem@gmail.com>
2013-11-20 21:26:08 +0530

    N7105: Update Fingerprint
    Change-Id: I1fccdee7a6b7d28d7da24bc48c04ef5e788af59d

--- carbon.mk
+++ carbon.mk
@@ -11,8 +11,12 @@ $(call inherit-product, vendor/carbon/config/common_phone.mk)
 PRODUCT_DEVICE := t0lte
 PRODUCT_NAME := carbon_t0lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := Note II
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := T0LTE
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lte TARGET_DEVICE=t0lte BUILD_FINGERPRINT="samsung/t0ltexx/t0lte:4.1.2/JZO54K/N7105XXDLL4:user/release-keys" PRIVATE_BUILD_DESC="t0ltexx-user 4.1.2 JZO54K N7105XXDLL4 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="samsung/t0ltexx/t0lte:4.3/JSS15J/N7105XXUENA1:user/release-keys" \
+    PRIVATE_BUILD_DESC="t0ltexx-user 4.3 JSS15J N7105XXUENA1 release-keys" \
+    PRODUCT_NAME=t0ltexx \
+    TARGET_DEVICE=t0lte
